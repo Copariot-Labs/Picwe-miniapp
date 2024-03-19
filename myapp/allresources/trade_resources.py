cfdb0b1e4936dbf639bf29960d5d96a16f0dc2f4
ShamirSecret <144866944+ShamirSecret@users.noreply.github.com>
2024-03-19 14:55:30 +0800

    Update trade_resources.py

--- myapp/allresources/trade_resources.py
+++ myapp/allresources/trade_resources.py
@@ -4,9 +4,9 @@ import okx.MarketData as MarketData
 import okx.Trade as Trade
 
 
-flag = "0" if status_of_pro == 0 else "1"  # live trading: 0, demo trading: 1
+flag = "0" 
 
-# 如果 status_of_pro 为 0，使用 live 的值，否则使用 demo 的值
+
 api_key = "" if status_of_pro == 0 else ""
 secret_key = "" if status_of_pro == 0 else ""
 passphrase = ""  
@@ -22,7 +22,7 @@ def get_price_from_okx(symbol):
         instId=symbol + "-USDT"
     )
     if not result:
-        return {'code': 404, 'message': '未找到价格', 'data': []}, 200
+        return {'code': 404, 'message': '1', 'data': []}, 200
     price = float(result['data'][0]['last'])
     return price
 
